f5a38963b3cb9fb2b20549c5dff9510d11289c0d
Martyna Kościukiewicz <martynakosciukiewicz@gmail.com>
2024-02-01 15:54:00 +0000

    Created the repo for python crash course learning and added files for string editing

---
+++ python_work/editing_strings.py
@@ -0,0 +1,27 @@
+# Add a tab to text
+print("Python")
+print("\tPython")
+
+# Add a newline
+print("Languages:\nPython\nC\nJavaScript")
+
+# Combine tabs and newlines
+print("Languages:\n\tPython\n\tC\n\tJavaScript")
+
+# To ensure no whitespace exists at the right side of a string use the r.strip() method
+favourite_language = 'python '
+print(favourite_language.rstrip())
+
+# left side - lstrip() 
+fav_language1 = ' python'
+print(fav_language1.lstrip())
+# both sides at once strip()
+fav_language2 = ' python '
+print(fav_language2.strip())
+
+# Remove prefixes with the removeprefix() method
+
+
+
+
+
