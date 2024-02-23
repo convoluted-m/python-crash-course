82350ceb31b82fe363d00a7985ba01285b77dab9
Martyna Kościukiewicz <martynakosciukiewicz@gmail.com>
2024-02-23 15:32:26 +0000

    New content for if statements

---
+++ python_work/Conditional-Tests/conditional_tests.py
@@ -0,0 +1,114 @@
+# checking for equality ==
+car = 'Mini Cooper'
+if car == 'Mini Cooper':
+    print(car)
+else:
+    print('\nFalse')
+
+# checking for inequality !=
+colour = 'red'
+if colour != 'red':
+    print("Not red!")
+else: 
+    print("\nIt's red!")
+
+# numerical comparisons
+answer = 4
+if answer == 5:
+    print("Well done!")
+else: 
+    print("\nThat's not the correct answer. Please try again!")
+
+age = 19
+if age > 18:
+    print("\nWelcome!")
+else:
+    print("\nSorry, you must be over 18 to view this conteny.")
+
+# multiple conditions - and/or
+age = 30
+age >=18 and age < 40 # can also use parentheses (age >=18) and (age < 40)
+
+# Checking if a Value Is in a List - in
+colours = ["sea green", "turquoise", "lime", "sage", "moss"]
+print("sea green" in colours)
+
+# Checking if a Value is not in a List - not in
+banned_users = ['Tom', 'David']
+user = 'Pat'
+if user not in banned_users:
+    print(f"{user}, welcome to the group!")
+
+good_moods = ["happy", "calm", "energetic"]
+mood = "calm"
+if mood in good_moods:
+    print("\nGlad you are feeling good today.")
+
+bad_moods = ["uneasy", "sad", "worried"]
+mood = "uneasy"
+if mood in bad_moods:
+    print ("I'm sorry you are feeling this way. Would you like to chat more about it?")
+
+# Boolean Expressions - True or False
+flower = "daffodil"
+print(flower == 'daffodil')
+
+
+# Simple if statement
+age = 19
+if age >= 18:
+    print("\nYou are old enough to vote!\n")
+
+
+# If statement with a for loop
+cars = ['audi', 'bmw', 'subaru', 'mini cooper']
+
+for car in cars:
+    if car == 'bmw':
+        print(car.upper())
+
+    else:
+        print(car.title())
+
+# if-else statements (execute one of two possible actions)
+age = 17
+if age >= 18:
+    print("You are old enough to vote!") 
+    print("Have you registered to vote yet?")
+else:
+    print("Sorry, you are too young to vote.")
+    print("Please register to vote as soon as you turn 18!\n")       
+
+# if-elif-else chain
+age = 12
+if age < 4:
+    print("Your admission cost is $0.")
+elif age < 18:
+    print("Your admission cost is $25.")
+else:
+    print("Your admission cost is $40.")
+
+# more concise
+age = 20
+if age < 4:
+    price = 0
+elif age < 18:
+    price = 25
+else:
+    price = 40
+print(f"\nYour admission cost is ${price}.")
+
+# multiple elif blocks
+age = 16
+if age < 16:
+    price = 10
+elif age < 20:
+    price = 25
+elif age < 65:
+    price = 10
+else:
+    price = 20
+print(f"The course cos is ${price}.")
+
+
+
--- python_work/Tuples/tuples.py
+++ python_work/Tuples/tuples.py
@@ -1,1 +1,33 @@
-# Tuples
+# # Tuples - immutable lists of values. Use when values should not be changed throughout the life of the program.
+# dimensions = (100, 50)
+# print(dimensions)
+# print(dimensions[0])
+# print(dimensions[1])
+
+# # looping through all values in a tuple
+# for dimension in dimensions:
+#     print(dimension)
+
+# # Writing over a tuple - reassigning the variable
+# dimensions = (100, 50)
+# print("Original dimensions:")
+# for dimension in dimensions:
+#     print(dimension)
+
+# dimensions = (200, 100)
+# print("\nModified dimensions:")
+# for dimension in dimensions:
+#     print(dimension)
+
+# Exercise 
+foods = ('sandwich', 'croissant', 'apple', 'banana', 'chocolate bar')
+print("\nThe buffet offers the following foods:")
+for food in foods:
+    print(food)
+
+# rewriting the tuple
+foods = ('sandwich', 'wrap', 'croissant', 'doughnut', 'apple')
+print("\nThe new buffet offer:")
+for food in foods:
+    print(food)
+
